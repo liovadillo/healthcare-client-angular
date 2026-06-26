aec44b0fcc5e3b5eefb21342f8a30f720c94f52d
evadillo <vadillotirado@gmail.com>
2026-06-26 12:35:51 -0600

    Added Patient Details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AppointmentDetail } from './components/appointment-detail/appointment-d
 import { DoctorsContainer } from './components/doctors-container/doctors-container';
 import { DoctorDetail } from './components/doctor-detail/doctor-detail';
 import { PatientContainer } from './components/patient-container/patient-container';
+import { PatientDetail } from './components/patient-detail/patient-detail';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'appointments', pathMatch: 'full' },
@@ -12,4 +13,5 @@ export const routes: Routes = [
   { path: 'doctors', component: DoctorsContainer },
   { path: 'doctors/:id', component: DoctorDetail },
   { path: 'patients', component: PatientContainer },
+  { path: 'patients/:id', component: PatientDetail },
 ];
---
+++ src/app/components/patient-detail/patient-detail.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PatientService } from '../../services/patient/patient';
+import { Observable } from 'rxjs';
+import { PatientDTO } from '../../models/patient/patient.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-patient-detail',
+  imports: [CommonModule],
+  templateUrl: './patient-detail.html',
+  styleUrl: './patient-detail.css',
+})
+export class PatientDetail implements OnInit {
+
+  patient$: Observable<PatientDTO> | null = null;
+  
+  constructor(
+    private router: Router,
+    private patientService: PatientService,
+    private route: ActivatedRoute
+  ){} 
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.patient$ = this.patientService.getById(id);
+  }
+
+  goBack(): void{
+    this.router.navigate(['/patients']);
+  }
+
+}
